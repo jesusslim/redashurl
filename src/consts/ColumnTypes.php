6ef8b5709af1070c0ff8ee7c19d6e17b931aca21
Slim <755808379@qq.com>
2018-07-18 14:40:26 +0800

    fst cmt

---
+++ src/consts/ColumnTypes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jesusslim
+ * Date: 2018/7/18
+ * Time: 下午2:17
+ */
+
+namespace redashurl\consts;
+
+
+class ColumnTypes
+{
+
+    const INTEGER = 'integer';
+    const FLOAT = 'float';
+    const BOOLEAN = 'boolean';
+    const STRING = 'string';
+    const DATETIME = 'datetime';
+    const DATE = 'date';
+
+}
